76d86c5f39af5796e0a51f7697123c1311c213f9
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2021-10-01 15:21:00 +0530

    update

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function clickHandler() {
 
 var sum = 0;
 
-var textArea = document.querySelector("#output-text-area");
-console.log(textArea);
+// var textArea = document.querySelector("#output-text-area");
+// console.log("textArea:", textArea);
 
 function calculateSum(dob) {
   for (i = 0; i < dob.length; i++) {
@@ -20,19 +20,17 @@ function calculateSum(dob) {
   checkLucky(sum);
 }
 
-// var sumOfDate = calculateSum();
-
-// checkLucky();
-
 function checkLucky(sumOfBday) {
   var inputLuckyNumber = document.querySelector("#input-luckyNumber");
   var userLuckyNumber = inputLuckyNumber.value;
+  var divOutput = document.querySelector("#div-output");
 
   console.log("Lucky Number " + userLuckyNumber);
   if (sumOfBday % userLuckyNumber === 0) {
-    console.log("bday lucky hai");
+    divOutput.innerText = "Your bday is Lucky";
   } else {
     console.log("nahi hai");
+    divOutput.innerText = "Your bday is not Lucky";
   }
   sum = 0;
 }
